8b863f37e6cd5ac9099c26837b73d0065be4a8a6
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-07 17:30:03 +0600

    Added the handler to change user role to system admin (only for system admin)

--- internal/http/routes/user.go
+++ internal/http/routes/user.go
@@ -25,7 +25,7 @@ func RegisterUserRoutes(r *mux.Router, secretKey string) {
 
 	users.Handle("/{user_id}/role", middleware.RoleMiddleware("system_admin", "admin")(http.HandlerFunc(handlers.UserRoleChangeHandler))).Methods(http.MethodPost)
 
-	users.Handle("/{user_id}/promote/admin", middleware.PermissionMiddleware(database.DB, "user:promote:admin")(http.HandlerFunc(handlers.CheckHealth))).Methods(http.MethodPost)
+	users.Handle("/{user_id}/promote/admin", middleware.PermissionMiddleware(database.DB, "user:promote:admin")(http.HandlerFunc(handlers.UserPromoteAdminHandler))).Methods(http.MethodPost)
 
 	users.Handle("/{user_id}/promote/moderator", middleware.PermissionMiddleware(database.DB, "user:promote:moderator")(http.HandlerFunc(handlers.CheckHealth))).Methods(http.MethodPost)
 
